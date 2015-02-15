d0810e91aa6482b8893e8b040ef26237d6c0763c
Kate <kbalducci@hotmail.com>
2015-02-15 11:01:46 -0600

    updated aws info

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
+  validates_presence_of :user_name, :full_name
 end
